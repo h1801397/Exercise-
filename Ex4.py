b8118f32a8730e9b8e3ed095afa7b46dc39fe21d
h1801397 <43990446+h1801397@users.noreply.github.com>
2018-10-30 22:45:22 +0000

    Add files via upload

---
+++ Ex4.py
@@ -0,0 +1,52 @@
+a = 33
+b = 200
+
+if b > a:
+  print("b is greater than a")
+
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+mark=int(input('Enter your mark:'))
+if mark >= 40:
+       print("you have passed this m0dule")
+else:
+       print("regretfuly, you have failet")
+
+
+mark=int(input('Enter your mark:'))
+if mark >= 90:
+    print("your grate is A ")
+elif mark >= 80:
+    print("your grate is B")
+elif mark >= 70:
+    print("your grate is C")
+elif mark >= 60:
+    print("your grate is D")
+else :
+    print("your grate is F")
+
+
+
+
+
+    
+
+ 
+       
